6938341dace53803393e239793b7cfca658e358f
liza-i <45426013+liza-i@users.noreply.github.com>
2021-08-23 11:23:24 +0300

    Update browser_history.sql

--- browser_history.sql
+++ browser_history.sql
@@ -2,7 +2,7 @@ create database browser_history
 
 create table if not exists users (
 	id serial primary key ,
-	username varchar (50) not null,
+	username varchar (50) not null unique,
 	password varchar (50) not null
 );
 
